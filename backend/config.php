78e8b37710f007943df98efe799791d9532ba9eb
Igor <oliveira.cardoso.igor0@gmail.com>
2026-03-16 19:57:50 -0300

    Config.php Local ou Hostinger

--- backend/config.php
+++ backend/config.php
@@ -1,9 +1,18 @@
 <?php
 date_default_timezone_set('America/Sao_Paulo');
 $db_host = 'localhost';
-$db_name = 'top_sorte';
-$db_user = 'root'; // default XAMPP
-$db_pass = '';     // default XAMPP
+
+if ($_SERVER['SERVER_ADDR'] == '127.0.0.1' || $_SERVER['SERVER_ADDR'] == '::1' || $_SERVER['HTTP_HOST'] == 'localhost') {
+    // Local (XAMPP)
+    $db_name = 'top_sorte';
+    $db_user = 'root';
+    $db_pass = '';
+} else {
+    // Servidor (Produção)
+    $db_name = 'u422005024_riffas';
+    $db_user = 'u422005024_riffas';
+    $db_pass = 'S3creta@';
+}
 
 try {
     // Connect to mysql server first to create db if needed
